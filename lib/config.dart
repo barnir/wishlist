e8d25cc08fdbf55e25979249eb5d80ae0b8830a5
FranciscoGuerra <franciscojpguerra@hotmail.com>
2025-08-12 17:48:40 +0100

    fix: update Google Sign-In client ID and improve authentication error handling

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,6 @@
 class Config {
   static const String cloudinaryCloudName = 'dqwh1uk68';
   static const String cloudinaryUploadPreset = 'wishlists_img';
-static const String googleSignInServerClientId = '515293340951-94s0arso1q5uciton05l3mso47709dia.apps.googleusercontent.com';
+ static const String googleSignInServerClientId = '515293340951-6bohghthee4qm98navijn4ku19rteq4v.apps.googleusercontent.com';
 }
 
