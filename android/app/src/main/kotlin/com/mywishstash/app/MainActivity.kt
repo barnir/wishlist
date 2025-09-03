e6319b0729d9352e461f28713bcbfc4bfeacbb34
FranciscoGuerra <franciscojpguerra@hotmail.com>
2025-09-03 15:01:49 +0100

    feat: add MainActivity class for Flutter embedding initialization

---
+++ android/app/src/main/kotlin/com/mywishstash/app/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.mywishstash.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    // Intentionally minimal. Flutter embedding will initialize the engine.
+}
